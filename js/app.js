f8524b8343ab3c4a6977f3c7f8531b2e4d45f19b
josh1502-bit <72353245+josh1502-bit@users.noreply.github.com>
2021-05-26 11:10:04 -0400

    app.js - funciones de escritura hecha, documentacion y algo de orden

--- js/app.js
+++ js/app.js
@@ -1,27 +1,128 @@
-var validCharacters = [8, 13, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
-var num = 18.7
+/* 
+-Nombre: App.js 
+-Version: 0.1
+-Funcion principal: calculadora
+-Descripcion detallada: 
+    Programa en js encargado de la escritura
+    en pantalla y en calcular el 'input' de la
+    de la calculador y retonar una output que
+    sera escrito en la seccion de 'result' del
+    HTML 'index.html'.
+-Diccionario:
+    *Del/del = Delete
+    *Ops = Operadores (+-*)
+*/
 
-// Opera la funcion
-window.onload = function() {
-    document.onkeypress = mostrarInformacionCaracter;
+//Variables preliminares
+var input = '';
+var validCharacters = [42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
+var validOpsMaster = [8, 13];
+
+// Funciones de operaciones (+-*/)
+var ops = {
+    plus: function plusNumbers(n1, n2) {
+        return (parseFloat(n1) + parseFloat(n2));
+    },
+
+    subtract: function subtractNumbers(n1, n2) {
+        return (parseFloat(n1) - parseFloat(n2));
+    },
+
+    multiply: function multiplyNumbers(n1, n2) {
+        return (parseFloat(n1) * parseFloat(n2));
+    },
+
+    divide: function divideNumbers(n1, n2) {
+        return (parseFloat(n1) / parseFloat(n2));
+    }
 }
 
-// Funcion de escritura
-function mostrarInformacionCaracter(evObject) {
+// Escribe en la pantalla
+function writeOnScreen() {
+    //Ver ifica si hay que escribir el cero predeterminado
+    if (input == '') {
+        control.innerHTML = '0';
+    } else {
+        //Escribe el input en el html
+        control.innerHTML = input;
+
+    }
+}
+
+// Opera las funciones de escritura en el HTML por parte del KeyBoard
+window.onload = function() {
+    document.onkeypress = writeFromKeyboard;
+    document.onkeyup = opsMaster;
+}
+
+// Funcion de escritura del teclado
+function writeFromKeyboard(evObject) {
 
     var msg = '';
-    var char = String.fromCharCode(evObject.which);
+    code = evObject.which; //|| evObject.keyCode;
+    // code = evObject.key;
+    char = ''
+    var char = String.fromCharCode(code);
 
     // "Deleteable"
     // Prueba en la consola para verificar que tecla se toca
-    console.log(char + ' <' + evObject.which + '>');
+    // console.log(char + '<' + code + ' > ');
     // "Deleteable"
 
     //Verifica si es un caracter valido
-    if (validCharacters.includes(evObject.which)) {
+    if (validCharacters.includes(code)) {
         msg = char;
-
-        control.innerHTML += msg;
+        input += msg;
+        writeOnScreen();
     }
 
 }
+
+// Funcion de lectura de los operadores (Extension de writeFromKeyboard())
+// Se incluyen (Delete y Enter)
+function opsMaster(evObject) {
+    code = evObject.keyCode;
+    switch (code) {
+        case 8:
+            //Delete
+            del();
+            writeOnScreen();
+            break;
+        default:
+            break;
+    }
+}
+
+// Funcion de escritura de la screen
+function writeFromScreen(char) {
+    op = ['equals', 'ac', 'del']
+    if (op.includes(char)) {
+        switch (char) {
+            case 'ac':
+                clean();
+                break;
+            case 'del':
+                del();
+                writeOnScreen();
+            default:
+                break;
+        }
+    } else {
+        input += char;
+        writeOnScreen();
+    }
+}
+
+// Funcion de limpiado (Solo input)
+function clean() {
+    input = '';
+    writeOnScreen();
+}
+
+// Funcion de la tecla delete (keybord/btnScreen)
+function del() {
+    input = input.slice(0, -1)
+}
+
+// Se escribe al principio de la pantalla
+writeOnScreen();
